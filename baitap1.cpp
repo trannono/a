8f16a0b923bd0508b6b1729c179551b784194a63
trannono <120113976+trannono@users.noreply.github.com>
2025-03-03 13:44:12 +0700

    Update baitap1.cpp

--- baitap1.cpp
+++ baitap1.cpp
@@ -7,6 +7,7 @@ struct ps{
 };
 ps rutgonphanso(ps a)
 {
+	//ham rut gon phan so 
 	ps p;
 	p.t=a.t/__gcd(a.t,a.m);
 	p.m=a.m/__gcd(a.t,a.m);
@@ -15,17 +16,19 @@ ps rutgonphanso(ps a)
 void xuly()
 {
 	ps a;
+	//nhap vao phan so can rut gon
 	cin>>a.t>>a.m;
 	cout<<rutgonphanso(a).t<<"/"<<rutgonphanso(a).m<<endl;
 }
 int main(){ 
   	
  	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-   	
-   	freopen("inp.txt", "r", stdin);
+	//nhap xuat file
+    freopen("inp.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int t;
-    t=2;
+	//so luong phan so can xu ly
+    t=1;
     //cin>>t;
     while(t--)
     {
